622c64af0d3ed337841ea312f4aa18cebe297ede
nicolaslunaromero <nicolaslunaromero@gmail.com>
2024-07-22 17:43:13 +0000

    avance v2

--- src/components/cash/CashRegister.tsx
+++ src/components/cash/CashRegister.tsx
@@ -1,4 +1,5 @@
 import { Select, SelectItem } from "@nextui-org/react";
+import {Input} from "@nextui-org/react";
 
 const options = [
   { key: "cat", label: "Cat" },
@@ -9,9 +10,9 @@ const options = [
 
 export const CashRegister = () => {
   return (
-    <div className="bg-dark p-12">
+    <div className="bg-white drop-shadow-xl p-12 rounded-2xl m-6 w-2/5">
 
-      <div className="w-full mx-3  rounded-md !bg-white m-5 ">
+      <div className=" mx-3  rounded-md m-5 ">
         <h1 className="font-sans font-medium text-dark text-2xl ">Branch</h1>
         <Select placeholder="Enter Type" labelPlacement="outside" className="max-w-xs" disableSelectorIconRotation>
           {options.map((option) => (
@@ -20,7 +21,7 @@ export const CashRegister = () => {
         </Select>
       </div>
 
-      <div className="w-full mx-3 rounded-md !bg-white ">
+      <div className=" mx-3 rounded-md m-5 ">
         <h1 className="font-sans font-medium text-dark text-2xl ">Cash Machine</h1>
         <Select placeholder="Enter Type" labelPlacement="outside" className="max-w-xs" disableSelectorIconRotation>
           {options.map((option) => (
@@ -28,6 +29,23 @@ export const CashRegister = () => {
           ))}
         </Select>
       </div>
+
+      <div className=" mx-3 rounded-md m-5 w-80">
+        <h1 className="font-sans font-medium text-dark text-2xl ">Open Ammount</h1>
+
+        <Input
+          type="number"
+
+          placeholder="0.00"
+          labelPlacement="outside"
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-400 text-small">$</span>
+            </div>
+          }
+        />
+      </div>
+
     </div>
   );
 };
