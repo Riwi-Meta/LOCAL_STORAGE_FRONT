c8b65fd5fe8c59583263ccbfd37ee9ebc4624de5
Carlox <carlito1999@live.com>
2024-07-22 19:09:49 -0500

    card innactive finished (only design)

--- src/components/discounts/DiscounstAdmin.tsx
+++ src/components/discounts/DiscounstAdmin.tsx
@@ -176,12 +176,12 @@ export const DiscounstAdmin: React.FC<props> = () => {
               {/* contenedor de nombre de producto,icono editar y codigo de producto*/}
               <div className="w-full h-auto flex flex-col">
                 <div className="flex justify-between">
-                  <p className="text-white font-bold productNameDiscountCustom">
-                    producto
+                  <p className="text-white font-bold productNameDiscountCustom text-3xl">
+                    Discount on Rice
                   </p>
                   <svg
-                  // onClick={} 
-                  className="size-9"
+                    // onClick={}
+                    className="size-8"
                     viewBox="0 0 24 24"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -204,14 +204,68 @@ export const DiscounstAdmin: React.FC<props> = () => {
                     </g>
                   </svg>
                 </div>
-                <p className="codeProductCustom">codigo</p>
+                <p className="text-gray codeProductCustom font-bold">
+                  code:DR-01655
+                </p>
               </div>
-              <p className="valueAssignedToProductCustom bg-orange-600">56%</p>
+              <p className="valueAssignedToProductCustom  text-white font-bold text-xl">
+                -5.25%
+              </p>
               <div className="w-full h-auto flex justify-between">
-                <p className="stateDiscountCustom bg-lime-800">estado</p>
-                <i className="deleteDicountCardCustom bg-red-700">
-                  icono borrar
-                </i>
+                <p className="stateDiscountCustom text-orange text-3xl font-bold">
+                  Inactive
+                </p>
+                <svg
+                  className="size-8"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      d="M4 6H20L18.4199 20.2209C18.3074 21.2337 17.4512 22 16.4321 22H7.56786C6.54876 22 5.69264 21.2337 5.5801 20.2209L4 6Z"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                    <path
+                      d="M7.34491 3.14716C7.67506 2.44685 8.37973 2 9.15396 2H14.846C15.6203 2 16.3249 2.44685 16.6551 3.14716L18 6H6L7.34491 3.14716Z"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                    <path
+                      d="M2 6H22"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                    <path
+                      d="M10 11V16"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                    <path
+                      d="M14 11V16"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </g>
+                </svg>
               </div>
             </div>
           </div>
