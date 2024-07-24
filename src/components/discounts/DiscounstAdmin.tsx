a62c1c918089f9cb7f81bb02e5947a0668ac626b
Carlox <carlito1999@live.com>
2024-07-23 19:07:20 -0500

    modal beta

--- src/components/discounts/DiscounstAdmin.tsx
+++ src/components/discounts/DiscounstAdmin.tsx
@@ -4,6 +4,7 @@ import {
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
+  Input,
   Modal,
   ModalBody,
   ModalContent,
@@ -15,15 +16,23 @@ import {
 import { Slider } from "@nextui-org/react";
 import { CardDiscountInactive } from "./CardDiscountInactive";
 import { CardDiscountActive } from "./CardDiscountActive";
+import { DateInput } from "@nextui-org/react";
+import { parseDate, getLocalTimeZone } from "@internationalized/date";
+import { useDateFormatter } from "@react-aria/i18n";
 import React from "react";
 export const DiscounstAdmin: React.FC = () => {
   const [useStatus, setUseStatus] = React.useState(false);
 
-  const lupaOn = useStatus ? " animate-Giro rotate-[90deg]" : "animate-Rotar  rotate-[0deg]";
-  const useStatusOn=()=>{
+  const lupaOn = useStatus
+    ? " animate-Giro rotate-[90deg]"
+    : "animate-Rotar  rotate-[0deg]";
+  const useStatusOn = () => {
     setUseStatus(!useStatus);
   };
 
+  const [value, setValue] = React.useState(parseDate("2024-04-04"));
+
+  let formatter = useDateFormatter({ dateStyle: "full" });
 
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
@@ -47,15 +56,15 @@ export const DiscounstAdmin: React.FC = () => {
                 <path
                   d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
                   stroke="#BEBEBE"
-                  stroke-width="2"
+                  stroke-width="2.5"
                   stroke-linecap="round"
                   stroke-linejoin="round"
                 ></path>
               </g>
             </svg>
             <input
-            onFocus={useStatusOn}
-              className="placeholder:italic placeholder:font-semibold placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-3xl py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-1 sm:text-sm"
+              onFocus={useStatusOn}
+              className="placeholder:font-semibold block bg-white w-full border-3 border-gray rounded-3xl py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-gray sm:text-sm"
               placeholder="Search"
               type="text"
               name="search"
@@ -138,6 +147,7 @@ export const DiscounstAdmin: React.FC = () => {
           </Dropdown>
         </div>
       </div>
+      {/* contenedor de tarjetas y boton para agregar nuevo descuento */}
       <div className="w-3/4">
         <div className="w-full h-[25%] gap-8 grid grid-cols-3">
           <div
@@ -192,26 +202,121 @@ export const DiscounstAdmin: React.FC = () => {
           </div>
           <CardDiscountInactive />
           <CardDiscountActive />
-          <div className="bg-white rounded-3xl p-6 shadow-md">jelou</div>
-          <div className="bg-white rounded-3xl p-6 shadow-md">jelou</div>
+          <CardDiscountInactive />
+          <CardDiscountActive />
+          <CardDiscountActive />
         </div>
-        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-          <ModalContent>
+        <Modal className="fixed flex justify-center" 
+        isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent className="p-4 w-max flex h-max rounded-3xl justify-center gap-5">
             {(onClose) => (
               <>
-                <ModalHeader className="flex flex-col gap-1">
-                  Modal Title
+                <ModalHeader className="flex flex-col text-4xl">
+                  New Discount
                 </ModalHeader>
-                <ModalBody>
-                  {/* contenido  */}
-                  <span>contenido</span>
+                <ModalBody className="flex flex-col w-full h-max">
+                  <div className="w-full flex ">
+                    <div className="w-full h-[6%] gap-8 grid grid-cols-2">
+                      <div className="bg-default-400 ">
+                        Name
+                        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                          <Input type="string" label="Enter Stock" />
+                        </div>
+                      </div>
+                      <div className="bg-default-400 ">
+                        Ammount
+                        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                          <Input
+                            type="number"
+                            placeholder="000.000,00"
+                            labelPlacement="outside"
+                          />
+                        </div>
+                      </div>
+                      <div className="bg-default-400 ">
+                        Type
+                        <Dropdown>
+                          <DropdownTrigger
+                            className="w-full justify-between"
+                            endContent={
+                              <svg
+                                className="size-6"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                style={{ transform: "rotate(270deg)" }}
+                              >
+                                <path fill="#202020" d="M16 19L5 12l11-7z" />
+                              </svg>
+                            }
+                          >
+                            <Button className="bg-casiWhite text-gray font-bold">
+                              Enter Type
+                            </Button>
+                          </DropdownTrigger>
+                          <DropdownMenu aria-label="Static Actions">
+                            <DropdownItem>Active</DropdownItem>
+                            <DropdownItem>Inactive</DropdownItem>
+                          </DropdownMenu>
+                        </Dropdown>
+                      </div>
+                      <div className="bg-default-400 ">
+                        Status
+                        <Dropdown>
+                          <DropdownTrigger
+                            className="w-full justify-between"
+                            endContent={
+                              <svg
+                                className="size-6"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                style={{ transform: "rotate(270deg)" }}
+                              >
+                                <path fill="#202020" d="M16 19L5 12l11-7z" />
+                              </svg>
+                            }
+                          >
+                            <Button className="bg-casiWhite text-gray font-bold">
+                              Select Status
+                            </Button>
+                          </DropdownTrigger>
+                          <DropdownMenu aria-label="Static Actions">
+                            <DropdownItem>Active</DropdownItem>
+                            <DropdownItem>Inactive</DropdownItem>
+                          </DropdownMenu>
+                        </Dropdown>
+                      </div>
+                      <div className="bg-default-400 ">
+                        Date
+                        <div className="w-full flex flex-row gap-2">
+                          <div className="w-full flex flex-col gap-y-2">
+                            <DateInput
+                              label="Date (controlled)"
+                              value={value}
+                              onChange={setValue}
+                            />
+                            <p className="text-default-500 text-sm">
+                              Selected date:{" "}
+                              {value
+                                ? formatter.format(
+                                    value.toDate(getLocalTimeZone())
+                                  )
+                                : "--"}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-3/4"></div>
                 </ModalBody>
                 <ModalFooter>
-                  <Button color="danger" variant="light" onPress={onClose}>
-                    Close
+                  <Button className="rounded-md"
+                  color="danger" onPress={onClose}>
+                    Cancel
                   </Button>
-                  <Button color="primary" onPress={onClose}>
-                    Action
+                  <Button className="rounded-md" 
+                  color="primary" onPress={onClose}>
+                    Create Discount
                   </Button>
                 </ModalFooter>
               </>
